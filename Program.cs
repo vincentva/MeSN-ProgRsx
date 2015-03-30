320a93651d5b73cef01d12ece25f5ca48bc500b2
Vincent Van Assche <vincent.vanassche@unicaen.fr>
2015-03-30 17:12:27 +0200

    Archivage initial du module UdpEchoServer

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;	// For IPEndPoint
+using System.Net.Sockets;	// For UdpClient, SocketException
+using System.IO; // Pour les fichiers
+
+namespace UdpEchoServer
+{
+	class UdPEchoServer
+	{
+		public static void Main (string[] args)
+		{
+			if (args.Length > 1) {// Test for correct # of args
+				throw new ArgumentException ("Parameters: <Port>");
+			}
+
+			int servPort = (args.Length == 1) ? Int32.Parse (args [0]) : 7;
+
+			FileInfo fichierLog = new FileInfo ("LogEchoUdp_" + DateTime.Now.Date.ToString ("dd-MM-yy") + ".txt");
+			ServeurEchoUdp serveur = new ServeurEchoUdp (servPort, fichierLog);
+
+			serveur.RenvoyerEchoEnBoucle ();
+
+
+
+		}
+	}
+}
---
+++ ServeurEchoUdp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace UdpEchoServer
+{
+	public class ServeurEchoUdp
+	{
+		private UdpClient serveurUdp = null;
+		private FileInfo fichierLog = null;
+		IPEndPoint remoteIPEndPoint = new IPEndPoint (IPAddress.Any, 0);
+		private bool arretServeur = false;
+		public bool ArretServeur {
+			set {
+				arretServeur = value;
+			}
+		}
+
+		public ServeurEchoUdp (int numPort,FileInfo fichierLog)
+		{
+			serveurUdp = new UdpClient(numPort);
+			this.fichierLog = fichierLog;
+			StreamWriter streamLog = fichierLog.AppendText();
+			streamLog.WriteLine (DateTime.Now.ToString () + " - Création du serveur");
+			streamLog.Close ();
+		}
+
+		public void RenvoyerUnEcho()
+		{
+			StreamWriter streamLog = null;
+			try{
+				streamLog = fichierLog.AppendText();
+				streamLog.WriteLine(DateTime.Now.ToString()+" - En attente de datagrammes");
+				byte[] byteBuffer = serveurUdp.Receive(ref remoteIPEndPoint);
+				streamLog.WriteLine(DateTime.Now.ToString()+" - Traitement du client " + remoteIPEndPoint);
+
+				streamLog.Write("\"" + System.Text.UTF8Encoding.UTF8.GetString(byteBuffer) + "\"");
+				serveurUdp.Send(byteBuffer, byteBuffer.Length, remoteIPEndPoint);
+				streamLog.WriteLine(" - echoed {0} bytes.", byteBuffer.Length);
+			}
+			catch (SocketException se) {
+				streamLog.WriteLine (se.ErrorCode + ": " + se.Message);
+			}
+			finally {
+				if (streamLog != null) {
+					streamLog.Close ();
+				}
+			}
+		}
+
+		public void RenvoyerEchoEnBoucle()
+		{
+			StreamWriter streamLog = fichierLog.AppendText();
+			streamLog.WriteLine (DateTime.Now.ToString () + " - Démarrage du serveur");
+			streamLog.Close ();
+			while (!arretServeur) {
+				RenvoyerUnEcho ();
+			}
+		}
+	}
+}
+
